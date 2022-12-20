76e80d585fd9155d5cdd5063666191fa4290c301
Giovanni <giosal21@gmail.com>
2022-12-20 19:13:35 +0100

    ese environment

---
+++ src/main/java/co/develhope/environmentvariables1/controllers/MainController.java
@@ -0,0 +1,16 @@
+package co.develhope.environmentvariables1.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @Autowired
+    Environment environment;
+    @GetMapping("/")
+    public String getVariables(){
+        return environment.getProperty("authCode")+environment.getProperty("devName");
+    }
+}
